f521b91fff1b9dc23f7dcd708ff4300fb6c90430
BogkoDima <bogko.dmitry@gmail.com>
2017-09-27 01:25:33 +0300

    Add files via upload

---
+++ ImageToTxt/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ImageToTxt
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Считывание изображения
+            Bitmap image = new Bitmap(@"D:\work\ImageToTxt\ImageToTxt\Resources\1.png");
+            //Bitmap image = new Bitmap(@"D:\work\ImageToTxt\PNG2.png");
+            
+            //Создается массив, в который будет записан конвертируемый файл
+            char[,] image_array_of_symbols = new char[image.Width, image.Height];
+
+            /*Основная часть программы, которая проходит по длине и высоте картинки 
+             * и получает цвет пикселя, что позволяет в зависимости от цвета пикселя
+             * указать каким символом он будем заменем для записи в файл
+            */
+            for (int x = 0; x < image.Width; x++)
+            {
+                for (int y = 0; y < image.Height; y++)
+                {             
+                    Color pixelColor = Color.FromArgb(image.GetPixel(x, y).ToArgb());
+
+                    if (Math.Abs(pixelColor.A - pixelColor.B) < 10 && Math.Abs(pixelColor.A - pixelColor.G) < 10 && Math.Abs(pixelColor.B - pixelColor.G) < 10 && pixelColor.A > 200)
+                    {
+                        image_array_of_symbols[x, y] = ' ';
+                    }
+                    else if (Math.Abs(pixelColor.A - pixelColor.B) < 10 && Math.Abs(pixelColor.A - pixelColor.G) < 10 && Math.Abs(pixelColor.B - pixelColor.G) < 10 && pixelColor.A < 100)
+                    {
+                        image_array_of_symbols[x, y] = '.';
+                    }
+                    else
+                    {
+                        if (pixelColor.A - pixelColor.B > 70 && pixelColor.A - pixelColor.G > 70)
+                        {
+                            image_array_of_symbols[x, y] = 'a';
+                        }
+                        else if (pixelColor.B - pixelColor.A > 70 && pixelColor.B - pixelColor.G > 70)
+                        {
+                            image_array_of_symbols[x, y] = 'y';
+                        }
+                        else
+                        {
+                            image_array_of_symbols[x, y] = 'x';
+                        }
+                    }
+                }
+            }
+            //Вывод массива в текстовый файл, в котором можно просмотреть полученый результат
+            using (StreamWriter outputFile = new StreamWriter("Output.txt"))
+            {                
+                    for (int i = 0; i < image.Height; i++)
+                    {
+                        for (int j = 0; j < image.Width; j++)
+                        {
+                            outputFile.Write(image_array_of_symbols[j, i]);
+                        }
+                    outputFile.Write("\n");
+                    }
+            }
+            
+            Application.Exit();
+        }
+
+        
+    }
+}
---
+++ ImageToTxt/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ImageToTxt
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+
+            
+        }
+    }
+}
